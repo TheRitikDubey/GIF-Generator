1313df26a42cf2912930b1a57f0bc6643be86767
TheRitikDubey <www.ritikdubey@gmail.com>
2023-04-07 09:31:01 +0530

    Prop delling is pending

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Navbar from "./components/Navbar";
 import Random from "./components/Random";
+import Tag from "./components/Tag";
 export default function App() {
   return (
     <>
-      <div className="w-full flex flex-col gap-10 h-screen background">
+      <div className="w-full flex flex-col gap-4 h-screen background">
           <Navbar/>
-          <div className="items-center w-full flex flex-col">
+          <div className="items-center w-full flex flex-col gap-8">
             <Random/>
+            <Tag/>
           </div>
-          
       </div>
     </>
     
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,3 +1,34 @@
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import Spinner from "./Spinner";
 export default function Tag() {
-  return <div className="">Tag</div>;
+  const [gif, setgif] = useState('');
+  
+  const [loader, setloader] = useState(false);
+  const API_KEY=process.env.REACT_APP_GIPHY_API_KEY
+  console.log("KEy of the API in tag is: ",API_KEY); //Undefined
+  console.log(API_KEY);
+  async function fetchData(){
+    setloader(true)
+    const url = `https://api.giphy.com/v1/gifs/Tag?api_key=${API_KEY}`;
+    const {data} = await axios.get(url);
+    console.log(data);
+    setloader(false);
+    const image_url=data.data.images.downsized_large.url;
+    console.log(image_url);
+
+  }
+  useEffect( () => {
+    fetchData();
+  },[])
+  function HandleGenerateGIF() {
+    fetchData()
+  }
+  return (
+      <div className="bg-green-500 mt-[25px] rounded-lg border-black border items-center gap-y-5  w-1/2 flex flex-col">
+          <h1 className="text-center text-2xl font-bold underline mt-10 uppercase">A Tag GIF</h1>
+            { loader === true? <Spinner/>:<img src={gif} alt="" width="400" />}
+            <button className="w-10/12 bg-white mb-6 p-4  opacity-80 rounded-md" onClick={HandleGenerateGIF}>Generate</button>
+       </div>
+  );
 }
